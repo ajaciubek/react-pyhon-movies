2a562d7530ea54377ccbb3c0fe577bde44095f29
Student <student@d17-327e-pc-13.ipa.ii.agh.edu.pl>
2025-01-25 13:49:10 +0100

    Delete movie

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState} from "react";
+import {useEffect} from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
 import MoviesList from "./MoviesList";
@@ -13,6 +14,17 @@ function App() {
     //     setAddingMovie(false);
     // }
 
+    useEffect(() => {
+    const fetchMovies = async () => {
+        const response = await fetch(`/movies`);
+        if (response.ok) {
+            const movies = await response.json();
+            setMovies(movies);
+        }
+    };
+    fetchMovies();
+    }, []);
+
     async function handleAddMovie(movie) {
       const response = await fetch('/movies', {
         method: 'POST',
@@ -25,13 +37,23 @@ function App() {
       }
     }
 
+    async function handleDeleteMovie(movie) {
+      const response = await fetch(`/movies/${movie.id}`, {
+        method: 'DELETE',
+      });
+        if (response.ok) {
+        const nextMovies = movies.filter(m => m !== movie);
+        setMovies(nextMovies);
+        }
+    }
+
     return (
         <div className="container">
             <h1>My favourite movies to watch</h1>
             {movies.length === 0
                 ? <p>No movies yet. Maybe add something?</p>
                 : <MoviesList movies={movies}
-                              onDeleteMovie={(movie) => setMovies(movies.filter(m => m !== movie))}
+                              onDeleteMovie={(movie) => handleDeleteMovie(movie)}
                 />}
             {addingMovie
                 ? <MovieForm onMovieSubmit={handleAddMovie}
